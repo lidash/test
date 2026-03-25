c5816ec83133ee36f6464ce9dcb37908170ba5df
lidash <lida.shtayer@gmail.com>
2026-03-25 12:15:23 +0200

    Fixed bug with linter

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,10 @@
 import unittest
 from calculator import add, subtract
 
+
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         self.assertEqual(add(2, 3), 5)
-
 
     def test_subtract(self):
         self.assertEqual(subtract(5, 2), 3)
@@ -12,4 +12,3 @@ class TestCalculator(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
