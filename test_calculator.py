78577d30b1d8ec57e09e72f1046445c5256c6ad0
lidash <lida.shtayer@gmail.com>
2026-03-25 12:08:23 +0200

    Introduce linter

--- test_calculator.py
+++ test_calculator.py
@@ -6,7 +6,7 @@ class TestCalculator(unittest.TestCase):
         self.assertEqual(add(2, 3), 5)
 
     def test_subtract(self):
-        self.assertEqual(subtract(5, 2), 2)
+        self.assertEqual(subtract(5, 2), 3)
 
 if __name__ == '__main__':
     unittest.main()
